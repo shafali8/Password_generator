6765d7ffef914371209e439fcc6ac3e18f79a5cf
Shafali Sonker <shafalisonker6@gmail.com>
2024-05-09 19:23:54 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,31 @@
+const passwordBox = document.getElementById("password");
+const length = 12;
+
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const number = "1234567890";
+const symbol = "!@#$%^&*";
+const allChar = upperCase + lowerCase + number + symbol;
+
+function createPassword() {
+    let password = ''
+    password += upperCase[(Math.floor(Math.random() * upperCase.length))];
+    password += lowerCase[(Math.floor(Math.random() * lowerCase.length))];
+    password += number[(Math.floor(Math.random() * number.length))];
+    password += symbol[(Math.floor(Math.random() * symbol.length))];
+
+    while(length > password.length){
+        password += allChar[Math.floor(Math.random() * allChar.length)];
+    }
+
+    
+passwordBox.value = password;
+}
+
+function copyPassword() {
+    passwordBox.select();
+    document.exeCommand("copy")
+}
+
+
+
